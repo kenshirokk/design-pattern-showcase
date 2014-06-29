814193ab0d5bc3e6fdb4ca9286f307add9bcbb8e
kenshiro <kenshirokk@gmail.com>
2014-06-29 21:06:01 +0800

    init

---
+++ builder/src/main/java/com/kk/designpattern/builder/App.java
@@ -0,0 +1,13 @@
+package com.kk.designpattern.builder;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
