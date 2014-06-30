d7631c5cb38a17550ff2ae1d753069ca61de237c
kenshiro <kenshirokk@gmail.com>
2014-06-30 23:56:20 +0800

    singleton

---
+++ singleton/src/main/java/com/kk/designpattern/singleton/Singleton.java
@@ -0,0 +1,20 @@
+package com.kk.designpattern.singleton;
+
+
+public class Singleton {
+
+	private static Singleton instance;
+	private Singleton(){}
+	
+	public static Singleton getInstance() {
+		if(null == instance) {
+			synchronized (Singleton.class) {
+				if(null == instance) {
+					instance = new Singleton();
+				}
+			}
+		}
+		return instance;
+	}
+	
+}
